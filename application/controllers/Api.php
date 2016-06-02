3fe9608189c25baa35994d64d995621e6998c6fd
Orann Weber <contact@orannweber.com>
2016-06-02 02:10:31 +0200

    init

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Api extends CI_Controller {
+class Api extends REST_Controller {
 
 	/**
 	 * Index Page for this controller.
